c1900a019d9f102b099f35a78e6fb138dea2ef7b
araki-shota2351780 <araki.shota0227@gmail.com>
2024-10-07 01:51:16 +0000

    変更

---
+++ 2-1/script/script.js
@@ -0,0 +1,13 @@
+// JavaScript用ファイル
+new Vue({
+    el: '#app'
+});
+
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            text: 'hello Vue.js'
+        };
+    }
+});
